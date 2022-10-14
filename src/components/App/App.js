ac2746c5e3dbd028fdb9899b84b5e395696cd409
Victoria Fields <victoriaashleyfields@gmail.com>
2022-10-14 16:49:47 -0600

    Create MovieContainer skeleton with props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import movieData from "../../mockData";
+import MovieContainer from "../MovieContainer/MovieContainer.js";
 
 class App extends React.Component {
   constructor() {
@@ -11,7 +12,14 @@ class App extends React.Component {
   }
 
   render() {
-    return <h1>Rancid Tomatillos</h1>;
+    return (
+      <main>
+        <h1>Rancid Tomatillos</h1>
+        <MovieContainer
+          movies={this.state.movies}
+        />
+      </main>
+    );
   }
 }
 
--- src/components/MovieContainer/MovieContainer.js
+++ src/components/MovieContainer/MovieContainer.js
@@ -1,4 +1,17 @@
 import React from "react";
 import "./MovieContainer.css";
+import MovieCard from "../MovieCard/MovieCard.js";
+
+function MovieContainer(props) {
+  const movieCards = props.movies.map(movie => {
+    return (
+      <MovieCard />
+    )
+  })
+
+  return (
+    <p>I am a container</p>
+  )
+}
 
 export default MovieContainer;
