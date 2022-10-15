8ba6349aad6299a8cf8bb0b00c72e706031133b4
Victoria Fields <victoriaashleyfields@gmail.com>
2022-10-15 13:50:08 -0600

    Refactor for fetch call to display single movie details

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -1,49 +1,53 @@
 import React from "react";
 import "./SingleMovie.css";
-import movieData from "../../mockData";
+// import movieData from "../../mockData";
 // import MovieCard from "../MovieCard/MovieCard.js";
 
-function SingleMovie(props) {
-  //to do convert to class component and change next line to api call
-  const movieDetails = movieData.movies.find(
-    (movie) => movie.id === props.movieId
-  );
-  console.log({ movieDetails });
-  return (
-    <div className="singleMovieBox">
-      {/* <p>{props.movieId}</p> */}
-      <img src={movieDetails.backdrop_path} className="backdrop" />
-      <div className="all-movie-details">
-        <img src={movieDetails.poster_path} className="single-poster" />
-        <div className="movie-details">
-          <h1>{movieDetails.title}</h1>
-          <h2>Rating: {movieDetails.average_rating.toFixed(1)}</h2>
-          <h2>
-            Release Date: {movieDetails.release_date.split("-").join("/")}
-          </h2>
+class SingleMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: {}
+    }
+  }
+
+  componentDidMount() {
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movieId}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          movie: data.movie
+        })
+      })
+  }
+
+  render() {
+    console.log(this.state.movie)
+    return (
+      <div className="singleMovieBox">
+        <img src={this.state.movie.backdrop_path} className="backdrop" />
+        <div className="all-movie-details">
+          <img src={this.state.movie.poster_path} className="single-poster" />
+          <div className="movie-details">
+            <h1>{this.state.movie.title}</h1>
+            <h2>Rating: {this.state.movie.average_rating}</h2>
+            <h2>
+              Release Date: {this.state.movie.release_date}
+            </h2>
+          </div>
         </div>
+        <button onClick={this.props.handleBackClick}>Back</button>
       </div>
-      <button onClick={props.handleBackClick}>Back</button>
-    </div>
-  );
+    );
+  }
+
 }
 
-// function SingleMovie(props) {
-//   const movieDetails = props.movies.map((movie) => {
-//     return (
-//       <MovieCard
-//         key={movie.id}
-//         id={movie.id}
-//         posterPath={movie.poster_path}
-//         backdropPath={movie.backdrop_path}
-//         averageRating={movie.average_rating}
-//         releaseDate={movie.release_date}
-//         title={movie.title}
-//       />
-//     );
-//   });
 
-//   return <section>{movieDetails}</section>;
-// }
+  //to do convert to class component and change next line to api call
+  // const movieDetails = movieData.movies.find(
+  //   (movie) => movie.id === props.movieId
+  // );
+  // console.log({ movieDetails });
 
 export default SingleMovie;
