d80248bb560d37d92504f52137fe8eda5b53d387
Dinne Kopelevich <dinne22@hotmail.com>
2022-10-21 14:15:29 -0600

    Refactor api calls

---
+++ src/apiCalls.js
@@ -0,0 +1,14 @@
+const checkError = (response) => {
+  if (!response.ok) {
+    throw new Error(`${response.status}`);
+  }
+  return response.json();
+};
+
+const getMovieData = (id = "") => {
+  return fetch(
+    `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`
+  ).then((response) => checkError(response));
+};
+
+export { getMovieData };
