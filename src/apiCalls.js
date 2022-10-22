ee2017cc89cbdb7283bab7f833fe5abaef48ff75
Dinne Kopelevich <dinne22@hotmail.com>
2022-10-22 17:53:59 -0600

    Add pointer hover]

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -11,10 +11,4 @@ const getMovieData = (id = "") => {
   ).then((response) => checkError(response));
 };
 
-const getMovieVideoData = (id = "") => {
-  return fetch(
-    `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`
-  ).then((response) => checkError(response));
-};
-
-export { getMovieData, getMovieVideoData };
+export { getMovieData };
